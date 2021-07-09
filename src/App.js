19ef308f7a2cbb667a44af25abb523a4867adb8c
Uyanhewajanadhi <uyanhewajandhi@gmail.com>
2021-07-09 22:29:10 +0530

    Commit with component folder

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Button from "@material-ui/core/Button";
 import Switch from "@material-ui/core/Switch";
 
 
-import WorkerLogIn from './WorkerLogIn.js';
-import AllTeams from './AllTeams.js';
-import Division from './Divisions.js';
-import SetupOrg from './setupOrg.js';
-import SetUpEmail from './setUpEmail.js';
-import ResponsiveDrawer from './ResponsiveDrawer.js';
+import WorkerLogIn from './components/admin/WorkerLogIn.js';
+// import AllTeams from './components/AllTeams.js';
+import Division from './components/admin/Divisions.js';
+import SetupOrg from './components/admin/setupOrg.js';
+import SetUpEmail from './components/admin/setUpEmail.js';
+import ResponsiveDrawer from './components/admin/ResponsiveDrawer.js';
 function App() {
 
 
--- src/components/admin/setUpEmail.js
+++ src/components/admin/setUpEmail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./App.css";
+import "../../App.css";
 import Button from "@material-ui/core/Button";
 import Card from '@material-ui/core/Card';
 import TextField from '@material-ui/core/TextField';
@@ -9,7 +9,7 @@ import OfficeImage from './resources/logo_big.png';
 function SetUpEmail() {
   return (
     <div className="App">
-     <header className="App-header">
+     <header >
         <Card className="Card-style">
         <img src={OfficeImage} className="VO-logo" alt="logo" />
         <p>
