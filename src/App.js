e7e22f0d99a3c15ffa32e129d320cca4873c4d4d
Naushikha <naushikha.jayawickrama@gmail.com>
2021-07-14 02:04:07 +0530

    Reverted backend URL to localhost

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Route from "./Routes";
 
-window.backendURL = "http://34.136.72.132:3030/api/v1";
+window.backendURL = "http://localhost:3030/api/v1";
 
 const App = () => {
   return <Route />;
