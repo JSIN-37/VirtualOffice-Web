ad6849f231ca72a2dab943288150197685a87b66
Naushikha <naushikha.jayawickrama@gmail.com>
2021-07-14 02:03:25 +0530

    Prepared for live frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 
 import Route from "./Routes";
-import AdminArea from "./AdminArea.js"
 
-window.backendURL = "http://localhost:3030/api/v1";
+window.backendURL = "http://34.136.72.132:3030/api/v1";
 
 const App = () => {
-  return <Route/>
+  return <Route />;
 };
 
 export default App;
