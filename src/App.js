0dde8e2d15cd67033e1971c1b87024a935da5dca
Naushikha <naushikha.jayawickrama@gmail.com>
2021-09-17 18:51:10 +0530

    Changed backend URL to live

--- src/App.js
+++ src/App.js
@@ -7,41 +7,41 @@ import AdminArea from "./AdminArea";
 import NotFound from "./components/NotFound";
 
 // Backend endpoint - set to live for now
-window.backendURL = "https://localhost:3030/api/v1";
+window.backendURL = "http://vo.zx-software.com:3040/api/v1"; // Define without trailing '/'
 
 const App = () => {
-    const [appD, setAppD] = React.useState({});
+  const [appD, setAppD] = React.useState({});
 
-    // Load saved credentials
-    React.useEffect(() => {
-        var credentials = {};
-        if (localStorage.getItem("credentials")) {
-            credentials = JSON.parse(localStorage.getItem("credentials"));
-            let tmpAppD = { email: credentials.email, token: credentials.token };
-            setAppD(tmpAppD);
-        }
-    }, []);
+  // Load saved credentials
+  React.useEffect(() => {
+    var credentials = {};
+    if (localStorage.getItem("credentials")) {
+      credentials = JSON.parse(localStorage.getItem("credentials"));
+      let tmpAppD = { email: credentials.email, token: credentials.token };
+      setAppD(tmpAppD);
+    }
+  }, []);
 
-    return (
-        <>
-            <Router>
-                <Switch>
-                    <Route exact path="/">
-                        <WelcomeScreen />
-                    </Route>
-                    <Route path="/employee">
-                        <EmployeeArea appD={appD} setAppD={setAppD} />
-                    </Route>
-                    <Route path="/admin">
-                        <AdminArea appD={appD} setAppD={setAppD} />
-                    </Route>
-                    <Route path="*">
-                        <NotFound />
-                    </Route>
-                </Switch>
-            </Router>
-        </>
-    );
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <WelcomeScreen />
+          </Route>
+          <Route path="/employee">
+            <EmployeeArea appD={appD} setAppD={setAppD} />
+          </Route>
+          <Route path="/admin">
+            <AdminArea appD={appD} setAppD={setAppD} />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
 };
 
 export default App;
