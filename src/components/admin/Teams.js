d96193763424928eb9133a20208abaca0ecef390
Imashi <2018is016@stu.ucsc.cmb.ac.lk>
2021-09-18 11:58:43 +0530

    minor changes to teams

--- src/components/admin/Teams.js
+++ src/components/admin/Teams.js
@@ -5,90 +5,90 @@ import { makeStyles } from "@material-ui/core/styles";
 import PeopleRoundedIcon from "@material-ui/icons/PeopleRounded";
 import { Tab } from "@material-ui/core";
 import { Tabs } from "@material-ui/core";
-import TeamOverview from "./TeamOverview";
-import AddTeam from "./AddTeam";
+import TeamOverview from "../employee/TeamOverview";
+import AddTeam from "../employee/AddTeam";
 import EditTeam from "./EditTeam";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    display: "flex",
-  },
-  apptitle: {
-    padding: theme.spacing(2), //16px
-    fontWeight: 500,
-    textDecoration: "none",
-  },
-  appbaricon: {
-    marginLeft: "240px",
-  },
-  appbar: {
-    background: "#E3E6F5",
-    height: 58,
-  },
-  appspace: {
-    padding: theme.spacing(2),//16px
-    fontWeight: 500,
-    color: "#E3E6F5"
-},
-tab: {
-  color: "#3F51B4",
-  textTransform: "none",
-  fontSize: "16px",
-},
-indicator: {
-  backgroundColor: "#3F51B4",
-},
+    root: {
+        width: "100%",
+        display: "flex",
+    },
+    apptitle: {
+        padding: theme.spacing(2), //16px
+        fontWeight: 500,
+        textDecoration: "none",
+    },
+    appbaricon: {
+        marginLeft: "240px",
+    },
+    appbar: {
+        background: "#E3E6F5",
+        height: 58,
+    },
+    appspace: {
+        padding: theme.spacing(2),//16px
+        fontWeight: 500,
+        color: "#E3E6F5"
+    },
+    tab: {
+        color: "#3F51B4",
+        textTransform: "none",
+        fontSize: "16px",
+    },
+    indicator: {
+        backgroundColor: "#3F51B4",
+    },
 }));
 
 export default function Teams() {
-  const classes = useStyles();
+    const classes = useStyles();
 
     const TabPanel = ({ children, index, value }) => {
-    return <>{value === index && <>{children}</>}</>;
-  };
+        return <>{value === index && <>{children}</>}</>;
+    };
 
-  const [value, setValue] = useState(0);
+    const [value, setValue] = useState(0);
     const handleTabs = (e, val) => {
         setValue(val);
     };
 
-  return (
-    <Grid container spacing={4}>
-      <AppBar
-        position="fixed"
-        color="primary"
-        className={classes.appbar}
-        elevation={0}
-      >
-        <Toolbar>
-          <PeopleRoundedIcon
-            color="primary"
-            className={classes.appbaricon}
-            fontSize="medium"
-          />
-          <Typography variant="h6" className={classes.apptitle} color="primary">
-            Teams
-          </Typography>
-          <Tabs value={value} onChange={handleTabs} classes={{ indicator: classes.indicator }} >
+    return (
+        <Grid container spacing={4}>
+            <AppBar
+                position="fixed"
+                color="primary"
+                className={classes.appbar}
+                elevation={0}
+            >
+                <Toolbar>
+                    <PeopleRoundedIcon
+                        color="primary"
+                        className={classes.appbaricon}
+                        fontSize="medium"
+                    />
+                    <Typography variant="h6" className={classes.apptitle} color="primary">
+                        Teams
+                    </Typography>
+                    <Tabs value={value} onChange={handleTabs} classes={{ indicator: classes.indicator }} >
                         <Tab label="Overview" className={classes.tab} />
                         <Tab label="Add Team" className={classes.tab} />
                         <Tab label="Edit Team" className={classes.tab} />
-          </Tabs>
+                    </Tabs>
 
-        </Toolbar>
-      </AppBar>
-      <TabPanel value={value} index={0}>
-        <TeamOverview/>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <AddTeam/>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <EditTeam/>
-      </TabPanel>
+                </Toolbar>
+            </AppBar>
+            <TabPanel value={value} index={0}>
+                <TeamOverview />
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <AddTeam />
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                <EditTeam />
+            </TabPanel>
 
-      
-    </Grid>
-  );
+
+        </Grid>
+    );
 }
--- src/components/employee/TeamOverview.js
+++ src/components/employee/TeamOverview.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Grid from '@material-ui/core/Grid'
-import Button from '@material-ui/core/Button'
+import { Container } from '@material-ui/core';
 import TeamCard from './TeamCard'
-import AddRoundedIcon from '@material-ui/icons/AddRounded';
 
 export default function TeamOverview() {
     const [teams, setTeams] = useState([])
@@ -21,17 +20,17 @@ export default function TeamOverview() {
         setTeams(newTeams)
     }
     return (
-        <Grid container spacing={3} ml={2}>
-            {
-                teams.map(team => (
-                    <Grid item xs={12} sm={6} md={4} key={team.id}>
-                        <TeamCard team={team} handleDelete={handleDelete} />
-                    </Grid>
-                ))
-            }
-            <Grid item md={12}>
-                <Button variant="contained" color="primary" startIcon={< AddRoundedIcon />}> Add Team </Button >
-            </Grid>
-        </Grid >
+        <Container>
+            <Grid container spacing={3} ml={2}>
+                {
+                    teams.map(team => (
+                        <Grid item xs={12} sm={6} md={4} key={team.id}>
+                            <TeamCard team={team} handleDelete={handleDelete} />
+                        </Grid>
+                    ))
+                }
+
+            </Grid >
+        </Container>
     )
 }
