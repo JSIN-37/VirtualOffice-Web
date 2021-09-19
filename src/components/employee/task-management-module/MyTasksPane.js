fbdb4f0e952685c0ddb76d1b04501757cf490aa1
Imashi <2018is016@stu.ucsc.cmb.ac.lk>
2021-09-20 03:30:56 +0530

    my task component styling

--- src/components/employee/task-management-module/MyTasksPane.js
+++ src/components/employee/task-management-module/MyTasksPane.js
@@ -6,33 +6,33 @@ import { makeStyles } from '@material-ui/core/styles';
 import TaskCardMini from './TaskCardMini';
 
 const useStyles = makeStyles({
-  pane: {
-    minHeight:'90vh',
-    maxHeight : '90vh',
-    overflow : 'auto'
-  },
+    pane: {
+        minHeight: '90vh',
+        maxHeight: '90vh',
+        overflow: 'auto'
+    },
 });
 
 export default function MyTasksPane(props) {
-  const tasks = props.tasks;
-  const handleTaskChange = props.handleTaskChange;
+    const tasks = props.tasks;
+    const handleTaskChange = props.handleTaskChange;
 
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Card className={classes.pane} variant="outlined" elevation={1}>
-      <CardHeader title='My Tasks'/>
-      <CardContent>
-        {tasks.map((task) => {
-          return (
-            <TaskCardMini
-              key={task.id}
-              task={task}
-              handleTaskChange={handleTaskChange}
-            />
-          );
-        })}
-      </CardContent>
-    </Card>
-  );
+    return (
+        <Card className={classes.pane} variant="outlined" elevation={1}>
+            <CardHeader title='My Tasks' />
+            <CardContent>
+                {tasks.map((task) => {
+                    return (
+                        <TaskCardMini
+                            key={task.id}
+                            task={task}
+                            handleTaskChange={handleTaskChange}
+                        />
+                    );
+                })}
+            </CardContent>
+        </Card>
+    );
 }
--- src/components/employee/task-management-module/TaskCardInspect.js
+++ src/components/employee/task-management-module/TaskCardInspect.js
@@ -5,28 +5,29 @@ import TaskDetails from './TaskDetails';
 import AddComment from './AddComment';
 
 const useStyles = makeStyles({
-  complete: {
-    height: '5vh',
-    marginTop: 10,
-  },
+    complete: {
+        height: '5vh',
+        marginTop: 20,
+        marginBottom: 10,
+    },
 });
 
 export default function TaskCardInspect() {
-  const classes = useStyles();
-  return (
-    <>
-      <TaskDetails />
-      <AddComment />
-      <Grid container justifyContent='center' alignItems='center'>
-        <Button
-          className={classes.complete}
-          variant='contained'
-          color='primary'
-        >
-          {' '}
-          Mark As Complete
-        </Button>
-      </Grid>
-    </>
-  );
+    const classes = useStyles();
+    return (
+        <>
+            <TaskDetails />
+            <Grid container justifyContent='center' alignItems='center'>
+                <Button
+                    className={classes.complete}
+                    variant='contained'
+                    color='primary'
+                >
+                    {' '}
+                    Mark As Complete
+                </Button>
+            </Grid>
+            <AddComment />
+        </>
+    );
 }
