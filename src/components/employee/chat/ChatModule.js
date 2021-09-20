115ad6f5a24ea0b5db232ab2c138c18245ad67b8
Imashi <2018is016@stu.ucsc.cmb.ac.lk>
2021-09-21 02:21:26 +0530

    styling for chat module and team tasks

--- src/components/employee/chat/ChatModule.js
+++ src/components/employee/chat/ChatModule.js
@@ -1,18 +1,35 @@
 import React from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+import { Container } from '@material-ui/core'
 import { ChatEngine } from 'react-chat-engine';
 import { AppData } from '../../../App'
 
-export default function ChatModule() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: "100%",
+        boxSizing: "border-box",
+    },
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+    },
+}));
 
+export default function ChatModule() {
+    const classes = useStyles();
     const [appD] = React.useContext(AppData)
-    console.log("IN chat module, user = ",appD)
+    console.log("IN chat module, user = ", appD)
     const userName = `${appD.user.first_name} ${appD.user.last_name}`
     return (
-        <ChatEngine
-			projectID='920a55aa-99cc-4f00-9ff9-9b660040956f'
-			userName ={userName}
-			userSecret='123'
-            height='70vh'
-		/>
+        <Container className={classes.root}>
+            <ChatEngine
+                projectID='920a55aa-99cc-4f00-9ff9-9b660040956f'
+                userName={userName}
+                userSecret='123'
+                height='85vh'
+                width='85vh'
+            />
+        </Container>
     )
 }
--- src/components/employee/team-task-stuff/TeamTaskCard.js
+++ src/components/employee/team-task-stuff/TeamTaskCard.js
@@ -1,37 +1,64 @@
 import React from "react";
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import {  Typography } from '@material-ui/core';
+//import CardHeader from '@material-ui/core/CardHeader';
+//import CardContent from '@material-ui/core/CardContent';
+import { Typography, Container, Grid } from '@material-ui/core';
+import AssignmentIcon from '@material-ui/icons/Assignment';
 import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
-    body : {
-        minWidth:'400px',
-        maxWidth : '400px',
-        minHeight:'100px',
-        maxHeight : '100px',
-        overflowY : 'auto'
+    body: {
+        minWidth: '400px',
+        maxWidth: '400px',
+        minHeight: '100px',
+        maxHeight: '100px',
+        overflowY: 'auto'
     },
-
-    card : {
-        marginTop: 20
-    }
+    card: {
+        //background: '#E3E6F5',
+        marginBottom: "12px",
+        marginLeft: "0",
+        padding: "10px 20px"
+    },
+    icon: {
+        fontSize: 40,
+        margin: "10px",
+    },
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+    },
 })
-
-
 
 export default function TeamTaskCard({ task }) {
     const classes = useStyles()
     return (
-        <Card className={classes.card}>
-            <CardHeader title={task.team} />
-            <CardContent className={classes.title}>
-                <Typography>{task.title}</Typography>
-            </CardContent>
-            <CardContent className={classes.body}>
-               <Typography> {task.description}</Typography>
-            </CardContent>
-        </Card>
+        <Container className={classes.container}>
+            <Card elevation={0} className={classes.card}>
+                <Grid container spacing={0}>
+                    <Grid item xs={2} >
+                        <AssignmentIcon className={classes.icon} color="primary" />
+                    </Grid>
+                    <Grid item xs={10}>
+                        <Typography variant="h6" pb={1}>
+                            {task.title}
+                        </Typography>
+                        <Typography variant="body1" pb={1}>
+                            {task.description}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Card >
+            {/*<Card className={classes.card}>
+    <CardHeader title={task.team} />
+    <CardContent className={classes.title}>
+        <Typography>{task.title}</Typography>
+    </CardContent>
+    <CardContent className={classes.body}>
+        <Typography> {task.description}</Typography>
+    </CardContent>
+    </Card>*/}
+        </Container >
     )
 }
